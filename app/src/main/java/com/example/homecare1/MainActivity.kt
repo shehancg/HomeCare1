9a093bc9fc2001af125869ba2fbe0389cd3aa89e
shehancg <shehanchamik@gmail.com>
2022-05-04 00:54:43 +0530

    1st Repo
    04-may

--- app/src/main/java/com/example/homecare1/MainActivity.kt
+++ app/src/main/java/com/example/homecare1/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.homecare1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private 
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth=Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSignUp.setOnClickListener{
+            val inputMethodManager=getSystemService(Context.INPUT_METHOD_SERVICE)as InputMethodManager
+
+        }
     }
+
 }
